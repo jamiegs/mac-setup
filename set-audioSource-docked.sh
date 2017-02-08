026e91ef97736a749be74a356043b9a07aff515a
Jamie Snell <jgsnell@gmail.com>
2017-02-08 10:28:36 -0600

    add system to set-audioSource-docked script

--- set-audioSource-docked.sh
+++ set-audioSource-docked.sh
@@ -1,2 +1,3 @@
 /usr/local/bin/switchaudiosource -t output -s "SteelSeries H Wireless"
 /usr/local/bin/switchaudiosource -t input -s "SteelSeries H Wireless"
+/usr/local/bin/switchaudiosource -t system -s "SteelSeries H Wireless"
